c8e0c18f8242a90dde3070ccc79183b2a214bc79
dung5ire <dung@5ire.org>
2023-03-09 19:41:40 +0700

    chore: fix transaction per byte
    Signed-off-by: dung5ire <dung@5ire.org>

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -25,19 +25,10 @@ pub mod currency {
 	pub const CENTS: Balance = 1_000 * MILLICENTS; // assume this is worth about a cent.
 	pub const DOLLARS: Balance = 1_000 * CENTS; // 1 DOLLARS = 1 5IRE
 
-	pub const FEES:Balance = 1_000_000_000_000_000;
-
-	pub const MICROCENTS: Balance = FEES / 1_0;
-
-	// pub const GASFEE:Balance = FEES/1_000_00;
-
 	pub const fn deposit(items: u32, bytes: u32) -> Balance {
 		items as Balance * 15 * CENTS + (bytes as Balance) * 6 * CENTS
 	}
 
-	pub const fn itemdeposit(items: u32, bytes: u32) -> Balance {
-		(items as Balance * 10 * CENTS/10 + (bytes as Balance) * (15 * MICROCENTS)) / 5
-	}
 }
 
 /// Time.
