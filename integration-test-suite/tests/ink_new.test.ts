813c58d1b56c48be26654138176257a9cf2829cd
dung@5ire.org <dung@5ire.org>
2023-06-05 09:30:50 +0700

    feat: add query and sing transaction test
    Signed-off-by: dung@5ire.org <dung@5ire.org>

--- integration-test-suite/tests/ink_new.test.ts
+++ integration-test-suite/tests/ink_new.test.ts
@@ -1,11 +1,15 @@
 import { expect } from "chai";
 import { BLOCK_TIME } from "../utils/constants";
 import { killNodes, polkadotApi, spawnNodes } from "../utils/util";
-import { CodePromise, Abi } from "@polkadot/api-contract";
+import { CodePromise, Abi, ContractPromise } from "@polkadot/api-contract";
 import { ApiPromise, Keyring } from "@polkadot/api";
-import abiFile from "./contracts/counter.json";
-
+import contractFile from "./contracts/counter.json";
+import type { WeightV2 } from "@polkadot/types/interfaces";
+import { BN} from "@polkadot/util";
 import { sleep, waitForEvent } from "../utils/setup";
+
+const MAX_CALL_WEIGHT = new BN(5_000_000_000);
+const PROOFSIZE = new BN(1_000_000);
 
 describe("Wasm test with new ink! version 4", function () {
   this.timeout(300 * BLOCK_TIME);
@@ -16,17 +20,54 @@ describe("Wasm test with new ink! version 4", function () {
     await spawnNodes();
   });
 
-  it("Should deploy a counter wasm contract to 5ire chain", async () => {
-    console.log("Beginning deploying wasm contract");
+  it("Should deploy/interact/query a counter wasm contract to 5ire chain", async () => {
+    console.log("Beginning deploying counter wasm contract");
 
-    let abi: string = JSON.stringify(abiFile);
+    const keyring = new Keyring({ type: "sr25519" });
+    const alice = keyring.addFromUri("//Alice");
 
-    let wasm = abiFile.source.wasm;
+    //const gasLimit = 100000n * 1000000n;
+    const gasLimit = polkadotApi.registry.createType('WeightV2', {
+        refTime: MAX_CALL_WEIGHT,
+        proofSize: PROOFSIZE,
+      }) as WeightV2;
+    
+    const storageDepositLimit = null;
+
+    let contractFileString: string = JSON.stringify(contractFile);
+
+    let wasm = contractFile.source.wasm;
     // deploy contract
-    await deployContract(polkadotApi, abi, wasm);
-
-    // wait for instantiated event
+    let contractAddress = await deployContract(
+      alice,
+      polkadotApi,
+      contractFileString,
+      wasm,
+      gasLimit,
+      storageDepositLimit
+    );
+    console.log("Address:", contractAddress);
     await waitForEvent(polkadotApi, "contracts", "Instantiated");
+    // interact contract with inc transaction
+    await incTransaction(
+      alice,
+      polkadotApi,
+      contractFileString,
+      contractAddress,
+      gasLimit,
+      storageDepositLimit
+    );
+    // query transaction
+    // await queryTransaction(
+    //   alice,
+    //   polkadotApi,
+    //   contractFileString,
+    //   contractAddress,
+    //   gasLimit,
+    //   storageDepositLimit
+    // );
+    // wait for instantiated event
+   
   });
 
   after(async () => {
@@ -35,44 +76,113 @@ describe("Wasm test with new ink! version 4", function () {
 });
 
 const deployContract = async (
+  alice: any,
   api: ApiPromise,
   contractFile: string,
-  contractWasm: string
+  contractWasm: string,
+  gasLimit: WeightV2,
+  storageDepositLimit: any
 ) => {
   // convert contract json file into usable contract ABI
-  let contractAbi = new Abi(contractFile, api?.registry?.getChainProperties());
+  //let contractAbi = new Abi(contractFile, api?.registry?.getChainProperties());
 
-  const contract = new CodePromise(api, contractAbi, contractWasm);
-
-  const gasLimit = 100000n * 1000000n;
-  const storageDepositLimit = null;
+  const code = new CodePromise(api, contractFile, contractWasm);
 
   let initValue = 0;
 
-  const keyring = new Keyring({ type: "sr25519" });
-  const alice = keyring.addFromUri("//Alice");
-  const tx = contract.tx.new(
+  const tx = code.tx.new(
     { gasLimit: gasLimit, storageDepositLimit: storageDepositLimit },
     initValue
   );
 
   let address: string;
 
-  const unsub = await tx.signAndSend(
+  address = await new Promise(async (resolve, reject) => {
+    await tx.signAndSend(
+      alice,
+      // @ts-ignore
+      ({ contract, status, dispatchError }) => {
+        if (status.isInBlock || status.isFinalized) {
+          address = contract.address.toString();
+          resolve(address);
+        }
+
+        if (dispatchError) {
+          console.log(`error occurred ${dispatchError}`);
+          reject(dispatchError);
+        }
+
+        expect(address).not.null;
+      }
+    );
+  });
+
+  return address;
+};
+
+const incTransaction = async (
+  alice: any,
+  api: ApiPromise,
+  contractFile: string,
+  contractAddress: string,
+  gasLimit: WeightV2,
+  storageDepositLimit: any
+) => {
+  console.log("Begin signing transaction smart contract");
+  // convert contract json file into usable contract ABI
+  //let contractAbi = new Abi(contractFile, api?.registry?.getChainProperties());
+  //console.log("Metadata:",contractAbi.json);
+  //Define deployed contract with metadata + contract address
+  const contract = new ContractPromise(api, contractFile, contractAddress);
+
+  
+  //Sign transaction
+  const tx = contract.tx.inc({
+    gasLimit: gasLimit,
+    storageDepositLimit: storageDepositLimit,
+  });
+  
+  await tx.signAndSend(
     alice,
     // @ts-ignore
-    ({ contract, status, dispatchError }) => {
-      if (status.isInBlock || status.isFinalized) {
-        address = contract.address.toString();
-        console.log("Contract address:",address);
-        unsub();
+    result => {
+      if (result.status.isInBlock || result.status.isFinalized) {
+        console.log("Block finalized");
+        
       }
 
-      if (dispatchError) {
-        console.log(`error occurred ${dispatchError}`);
-      }
-
-      expect(address).not.null;
     }
   );
+  console.log("here");
+};
+
+const queryTransaction = async (
+  alice: any,
+  api: ApiPromise,
+  contractFile: string,
+  contractAddress: string,
+  gasLimit: WeightV2,
+  storageDepositLimit: any
+) => {
+
+  console.log("Begin querying smart contract");
+
+  // convert contract json file into usable contract ABI
+  //let contractAbi = new Abi(contractFile, api?.registry?.getChainProperties());
+  //Define deployed contract with metadata + contract address
+  const contract = new ContractPromise(api, contractFile, contractAddress);
+
+  
+  // Query value from contract
+  const { result, output } = await contract.query.get(alice.address, {
+    gasLimit,
+    storageDepositLimit,
+  });
+  // check if the call was successful
+  if (result.isOk) {
+    // output the return value
+    console.log("Success -> Value:", output?.toHuman());
+  } else {
+    console.error("Error", result.asErr);
+  }
 };
