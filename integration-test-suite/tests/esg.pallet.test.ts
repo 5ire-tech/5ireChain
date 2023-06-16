04da6217b6fa035c1d725bbdda6a95d358488f8c
Polkaverse <pankajchaudhary172@gmail.com>
2023-06-16 15:14:30 +0530

    added esg pallet integration testing

---
+++ integration-test-suite/tests/esg.pallet.test.ts
@@ -0,0 +1,155 @@
+import { expect } from "chai";
+import { BLOCK_TIME } from "../utils/constants";
+import { killNodes, polkadotApi as api, spawnNodes } from "../utils/util";
+import { Keyring, WsProvider } from "@polkadot/api";
+import { KeyringPair } from "@polkadot/keyring/types";
+import { waitForEvent } from "../utils/setup";
+
+// Keyring needed to sign using Alice account
+const keyring = new Keyring({ type: "sr25519" });
+
+// This script contains the integration test for the ESG pallet.
+// ESG pallet is the pallet in 5ire-chain which is responsible to add the esg score and related transactions.
+
+describe("ESG Pallet Integration tests", function () {
+  this.timeout(300 * BLOCK_TIME);
+
+  before(async () => {
+    await spawnNodes();
+  });
+
+  // Should init
+  it("Should init and ESG Pallet", async () => {
+    const { alice, bob, charlie } = await init();
+
+    const esg_data = [
+      {
+        account: "5FLSigC9HGRKVhB9FiEo4Y3koPsNmBmLJbpXg2mp1hXcS59Y",
+        score: "43",
+      },
+    ];
+
+    const json_data = JSON.stringify(esg_data);
+
+    await registerOracle(alice, bob);
+
+    await insertEsgScores(bob, charlie, json_data);
+
+    await deRegisterOracle(alice, bob);
+  });
+
+  after(async () => {
+    await killNodes();
+  });
+});
+
+// Setup the API and Accounts
+async function init() {
+  const alice = keyring.addFromUri("//Alice", { name: "Alice default" });
+  const bob = keyring.addFromUri("//Bob", { name: "Bob default" });
+  const charlie = keyring.addFromUri("//Charlie", { name: "Charlie default" });
+
+  return { alice, bob, charlie };
+}
+
+// Register the Bob account as oracle in ESG pallet from ALICE(sudo account).
+async function registerOracle(alice: KeyringPair, bob: KeyringPair) {
+  console.log(`\n: Registering Oracle`);
+
+  const transaction = await api.tx.esgScore.registerAnOracle(bob.address, true);
+
+  const unsub = await api.tx.sudo
+    .sudo(transaction.method.toHex())
+    .signAndSend(alice, { tip: 200, nonce: -1 }, (result) => {
+      console.log(`Oracle Registration is ${result.status}`);
+      if (result.status.isInBlock) {
+        console.log(
+          `Oracle Registration at blockHash ${result.status.asInBlock}`
+        );
+        console.log(`Waiting for finalization... (can take a minute)`);
+      } else if (result.status.isFinalized) {
+        const data = JSON.stringify(result.events);
+        console.log(data);
+      }
+    });
+
+  await waitForEvent(api, "esgScore", "NewOracleRegistered");
+  const oracle_accounts = await api.query.esgScore.sudoOraclesStore();
+  expect(oracle_accounts.toString().includes(bob.address.toString()));
+  console.log(`Account verified in the oracle storage: ${oracle_accounts}`);
+
+  return true;
+}
+
+// Insert the ESG scores of the User by oracle account(bob) we added above.
+async function insertEsgScores(
+  bob: KeyringPair,
+  user: KeyringPair,
+  json_data: string
+) {
+  console.log(`\n Inserting ESG Score of the user.`);
+
+  const transaction = await api.tx.esgScore.upsertEsgScores(json_data);
+
+  const unsub = await transaction.signAndSend(
+    bob,
+    { tip: 200, nonce: -1 },
+    (result) => {
+      console.log(`Insertion of ESG score is ${result.status}`);
+      if (result.status.isInBlock) {
+        console.log(
+          `Insertion of ESG score at blockHash ${result.status.asInBlock}`
+        );
+        console.log(`Waiting for finalization... (can take a minute)`);
+      } else if (result.status.isFinalized) {
+        const data = JSON.stringify(result.events);
+        console.log(data);
+      }
+    }
+  );
+
+  await waitForEvent(api, "esgScore", "ESGStored");
+  const score = await api.query.esgScore.esgScoresMap(user.address);
+  expect(score.toString() == "43");
+  console.log(`ESG Score verified in storage: ${score}`);
+
+  return true;
+}
+
+// De-Register the Bob account as oracle in ESG pallet from ALICE(sudo account).
+async function deRegisterOracle(alice: KeyringPair, bob: KeyringPair) {
+  console.log(`\n: De-Registering Oracle`);
+
+  const existing_oracle_accounts = await api.query.esgScore.sudoOraclesStore();
+  expect(existing_oracle_accounts.toString().includes(bob.address.toString()));
+  console.log(
+    `Account verified in the oracle storage: ${existing_oracle_accounts}`
+  );
+
+  const transaction = await api.tx.esgScore.deregisterAnOracle(
+    bob.address,
+    true
+  );
+
+  const unsub = await api.tx.sudo
+    .sudo(transaction.method.toHex())
+    .signAndSend(alice, { tip: 200, nonce: -1 }, (result) => {
+      console.log(`Oracle De-Registration is ${result.status}`);
+      if (result.status.isInBlock) {
+        console.log(
+          `Oracle De-Registration at blockHash ${result.status.asInBlock}`
+        );
+        console.log(`Waiting for finalization... (can take a minute)`);
+      } else if (result.status.isFinalized) {
+        const data = JSON.stringify(result.events);
+        console.log(data);
+      }
+    });
+
+  await waitForEvent(api, "esgScore", "OracleDeRegistered");
+  const oracle_accounts = await api.query.esgScore.sudoOraclesStore();
+  expect(!oracle_accounts.toString().includes(bob.address.toString()));
+  console.log(`Account verified in the oracle storage: ${oracle_accounts}`);
+
+  return true;
+}
