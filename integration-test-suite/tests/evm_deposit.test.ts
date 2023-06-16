c37912774c12afeea209447dca79f659c0786285
Damilare <dharjeezy@gmail.com>
2023-06-16 06:06:25 +0100

    deposit and withdraw tests

--- integration-test-suite/tests/evm_deposit.test.ts
+++ integration-test-suite/tests/evm_deposit.test.ts
@@ -11,7 +11,7 @@ const keyring = new Keyring({ type: 'sr25519' });
 
 const ERC20_BYTECODES = require("./contracts/MyToken.json").bytecode;
 
-describe('EVM related tests', function () {
+describe('EVM deposit test', function () {
   this.timeout(300 * BLOCK_TIME);
 
   before(async () => {
