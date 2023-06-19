46d3a7deba4a2a86aa1aac601ea429b4b39d3ee4
dung5ire <dung@5ire.org>
2023-06-19 13:58:25 +0700

    chore : test transfer evm to evm
    Signed-off-by: dung5ire <dung@5ire.org>

--- integration-test-suite/tests/evmtransfer.test.ts
+++ integration-test-suite/tests/evmtransfer.test.ts
@@ -1,16 +1,15 @@
-import { expect } from 'chai';
-import { BLOCK_TIME } from '../utils/constants';
-import {killNodes, polkadotApi, spawnNodes} from "../utils/util";
-import {ApiPromise, Keyring, WsProvider} from "@polkadot/api";
-import {sudoTx, waitForEvent} from "../utils/setup";
-import { addressToEvm } from '@polkadot/util-crypto';
+import { expect } from "chai";
+import { BLOCK_TIME } from "../utils/constants";
+import { killNodes, polkadotApi, spawnNodes } from "../utils/util";
+import { ApiPromise, Keyring, WsProvider } from "@polkadot/api";
+import { sudoTx, waitForEvent } from "../utils/setup";
+import { addressToEvm } from "@polkadot/util-crypto";
 import { Web3 } from "web3";
 
-
 // Keyring needed to sign using Alice account
-const keyring = new Keyring({ type: 'sr25519' });
+const keyring = new Keyring({ type: "sr25519" });
 
-describe('EVM token tests', function () {
+describe("EVM token tests", function () {
   this.timeout(300 * BLOCK_TIME);
   // 4 session.
   this.slow(40 * BLOCK_TIME);
@@ -20,68 +19,88 @@ describe('EVM token tests', function () {
   });
 
   // Should transfer EVM token
-  it('Should transfer EVM tokens', async () => {
-    const {alice, aliceEthAccount, bob, bobEthAccount } = await init();
+  it("Should transfer EVM tokens", async () => {
+    const {
+      alice,
+      aliceEthAccount,
+      bob,
+      bobEthAccount,
+      testAccount,
+      testEthAccount,
+    } = await init();
+
     const web3 = new Web3(
       new Web3.providers.HttpProvider("http://127.0.0.1:9933")
     );
-    const BobaddressString = web3.utils.bytesToHex(bobEthAccount);
-    const AliceaddressString = web3.utils.bytesToHex(aliceEthAccount);
-    console.log("address 1" , BobaddressString);
-    console.log("address 2" , AliceaddressString);
+    const testAddressString = web3.utils.bytesToHex(testEthAccount);
+    const bobAddressString = web3.utils.bytesToHex(bobEthAccount);
+    //Create a extrinsic, transferring 10 5ire coin to test Account
+    // Because we can get privateKey through test Account
+    // subkey inspect "movie shock injury cliff envelope armed van lunar mail disease balance cigar"
+    const amount = polkadotApi.createType("Balance", "10000000000000000000");
+    const transaction = polkadotApi.tx.evm.deposit(testEthAccount, amount);
+
+    const unsub = await transaction.signAndSend(alice, (result) => {
+      console.log(`Swap is ${result.status}`);
+      if (result.status.isInBlock) {
+        console.log(`Swap included at blockHash ${result.status.asInBlock}`);
+        console.log(`Waiting for finalization... (can take a minute)`);
+      } else if (result.status.isFinalized) {
+        console.log(`events are ${result.events}`);
+        console.log(`Swap finalized at blockHash ${result.status.asFinalized}`);
+        unsub();
+      }
+    });
+
+    await waitForEvent(polkadotApi, "balances", "Transfer");
+    let testBalanceAfter = await web3.eth.getBalance(testAddressString);
+    let expectationBalanceAfter = web3.utils.toBigInt("10000000000000000000");
+    expect(testBalanceAfter).to.equal(expectationBalanceAfter);
+    const privateKeyTestAccount =
+      "2da33fc82c9b1e22a2a7d33ff963c0a8a528f6d88aec1ce181b398947ebfafdb";
+
+      const gasPrice = await web3.eth.getGasPrice();
+    const transferEVM = {
+      from: testAddressString,
+      to: bobAddressString,
+      value:"1000000000000000000",
+      gas: BigInt(21000),
+      gasPrice: gasPrice, //wei
+      nonce: await web3.eth.getTransactionCount(testAddressString, "pending"),
+    };
+    const gasAmount = await web3.eth.estimateGas(transferEVM);
+    let fee = Number((gasPrice * gasAmount) / BigInt(10 ** 18));
+    transferEVM.gas = gasAmount;
     
-;    let bobBalance = await web3.eth.getBalance(BobaddressString);
-    let AliceBalance = await web3.eth.getBalance(AliceaddressString);
-    console.log("Balance:",bobBalance);
-    console.log("Alice EVM Balance:", AliceBalance);
-    let expectationBalance = web3.utils.toBigInt(0);
-    //assert that bob initial evm balance is 0
-    expect(bobBalance).to.equal(expectationBalance);
-
-
-
-
-    //Tranfer between BobEthAccount and AliceEthAccount
-
-        var originalAmountToSend = '0.01';
-        var amountToSend = web3.utils.toWei(originalAmountToSend, 'ether');
-        var Tx = require('ethereumjs-tx');
-        var privateKey = Buffer.from('e5be9a5092b81bca64be81d212e7f2f9eba183bb7a90954f7b76361f6edb5c0a', 'hex')
-        var count= web3.eth.getTransactionCount(AliceaddressString);
-        var rawTx = {
-        from: AliceaddressString,
-        nonce: web3.utils.toHex(count),
-        gasPrice: web3.utils.toHex(5000000000),
-        gasLimit: web3.utils.toHex(220000),
-        to: BobaddressString,
-        value: web3.utils.toHex(amountToSend)
-        }
-
-        var tx = new Tx(rawTx);
-        tx.sign(privateKey);
-        var serializedTx = tx.serialize();
-        console.log("SerializedTX",serializedTx.toString('hex'));
-        // 0xf889808609184e72a00082271094000000000000000000000000000000000000000080a47f74657374320000000000000000000000000000000000000000000000000000006000571ca08a8bbf888cfa37bbf0bb965423625641fc956967b81d12e23709cead01446075a01ce999b56a8a88504be365442ea61239198e23d1fce7d00fcfc5cd3b44b7215f
-        var result = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'));
-        console.log("Result", result);
-    
-
+    const signedTx = await web3.eth.accounts.signTransaction(
+      transferEVM,
+      "0x2da33fc82c9b1e22a2a7d33ff963c0a8a528f6d88aec1ce181b398947ebfafdb"
+    );
+    const txInfo = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+    console.log("Here TxInfo", txInfo);
   });
   // Setup the API and Alice Account
-    async function init() {
-        const alice = keyring.addFromUri('//Alice', { name: 'Alice default' });
-        const bob = keyring.addFromUri('//Bob', { name: 'Bob default' });
-    
-        const aliceEthAccount = addressToEvm(alice.addressRaw);
-        const bobEthAccount = addressToEvm(bob.addressRaw);
-        return { alice, aliceEthAccount, bob, bobEthAccount };
-    }
+  async function init() {
+    const alice = keyring.addFromUri("//Alice", { name: "Alice default" });
+    const bob = keyring.addFromUri("//Bob", { name: "Bob default" });
+    const testAccount = keyring.addFromMnemonic(
+      "movie shock injury cliff envelope armed van lunar mail disease balance cigar"
+    );
 
+    const aliceEthAccount = addressToEvm(alice.addressRaw);
+    const bobEthAccount = addressToEvm(bob.addressRaw);
+    const testEthAccount = addressToEvm(testAccount.addressRaw);
+    return {
+      alice,
+      aliceEthAccount,
+      bob,
+      bobEthAccount,
+      testAccount,
+      testEthAccount,
+    };
+  }
 
   after(async () => {
     await killNodes();
   });
 });
-
-
-   
