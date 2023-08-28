6caf4f9875e994a7a03f58a479cb1f4aebce91ac
Damilare <dharjeezy@gmail.com>
2023-08-28 15:44:30 +0100

    format

--- frame/evm/test-vector-support/src/lib.rs
+++ frame/evm/test-vector-support/src/lib.rs
@@ -133,9 +133,7 @@ pub fn test_precompile_test_vectors<P: Precompile>(filepath: &str) -> Result<(),
 					);
 				}
 			},
-			Err(err) => {
-				return Err(format!("Test '{}' returned error: {:?}", test.name, err))
-			},
+			Err(err) => return Err(format!("Test '{}' returned error: {:?}", test.name, err)),
 		}
 	}
 
