dae1457090b3827691701d3fe537fb129c70ab3a
Ayushi <ayushi@5ire.org>
2023-10-21 01:01:22 +0530

    remove pallet-message-queue

--- runtime/firechain-uat-runtime/src/lib.rs
+++ runtime/firechain-uat-runtime/src/lib.rs
@@ -1213,26 +1213,6 @@ impl pallet_bounties::Config for Runtime {
 }
 
 parameter_types! {
-	/// Allocate at most 20% of each block for message processing.
-	///
-	/// Is set to 20% since the scheduler can already consume a maximum of 80%.
-	pub MessageQueueServiceWeight: Option<Weight> = Some(Perbill::from_percent(20) * RuntimeBlockWeights::get().max_block);
-}
-
-impl pallet_message_queue::Config for Runtime {
-	type RuntimeEvent = RuntimeEvent;
-	type WeightInfo = ();
-	/// NOTE: Always set this to `NoopMessageProcessor` for benchmarking.
-	type MessageProcessor = pallet_message_queue::mock_helpers::NoopMessageProcessor<u32>;
-	type Size = u32;
-	type QueueChangeHandler = ();
-	type QueuePausedQuery = ();
-	type HeapSize = ConstU32<{ 64 * 1024 }>;
-	type MaxStale = ConstU32<128>;
-	type ServiceWeight = MessageQueueServiceWeight;
-}
-
-parameter_types! {
 	pub const ChildBountyValueMinimum: Balance = DOLLARS;
 }
 
@@ -1928,7 +1908,6 @@ construct_runtime!(
 		RankedCollective: pallet_ranked_collective,
 		AssetConversion: pallet_asset_conversion,
 		FastUnstake: pallet_fast_unstake,
-		MessageQueue: pallet_message_queue,
 		Pov: frame_benchmarking_pallet_pov,
 		Statement: pallet_statement,
 		EsgScore: pallet_esg,
@@ -1982,7 +1961,6 @@ mod benches {
 		[pallet_im_online, ImOnline]
 		[pallet_indices, Indices]
 		[pallet_membership, TechnicalMembership]
-		[pallet_message_queue, MessageQueue]
 		[pallet_mmr, Mmr]
 		[pallet_multisig, Multisig]
 		[pallet_nomination_pools, NominationPoolsBench::<Runtime>]
