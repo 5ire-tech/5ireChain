fd3eebd50764abf468aca97e1c85684bc6b33c6f
dung5ire <dung@5ire.org>
2024-03-27 12:50:23 +0700

    feat: add test balance for evm
    Signed-off-by: dung5ire <dung@5ire.org>

--- integration-test-suite/utils/constants.ts
+++ integration-test-suite/utils/constants.ts
@@ -8,3 +8,7 @@ export const ACC2_PK =
 
 export const ETH_BLOCK_GAS_LIMIT = 75000000; // The same configuration as runtime
 export const ETH_BLOCK_POV_LIMIT = 5 * 1024 * 1024; // The same configuration as runtime
+
+export const GENESIS_ACCOUNT = "0xd43593c715fdd31c61141abd04a99fd6822c8558";
+export const GENESIS_ACCOUNT_BALANCE = "19342813113834066795298815";
+export const EXISTENTIAL_DEPOSIT = 0; // The minimum amount required to keep an account open
