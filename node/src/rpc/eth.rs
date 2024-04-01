228bae108df41857813b5808f6ca6075b293e911
dung5ire <dung@5ire.org>
2024-04-01 22:24:43 +0700

    chore: remove unused deps and update toolchain
    Signed-off-by: dung5ire <dung@5ire.org>

--- node/src/rpc/eth.rs
+++ node/src/rpc/eth.rs
@@ -17,9 +17,9 @@ use sp_blockchain::{Error as BlockChainError, HeaderBackend, HeaderMetadata};
 use sp_core::H256;
 use sp_runtime::traits::Block as BlockT;
 // Frontier
-pub use fc_rpc::{EthBlockDataCacheTask, EthConfig, OverrideHandle, StorageOverride};
+pub use fc_rpc::{EthBlockDataCacheTask, EthConfig, OverrideHandle};
 pub use fc_rpc_core::types::{FeeHistoryCache, FeeHistoryCacheLimit, FilterPool};
-pub use fc_storage::overrides_handle;
+// pub use fc_storage::overrides_handle;
 use fp_rpc::{ConvertTransaction, ConvertTransactionRuntimeApi, EthereumRuntimeRPCApi};
 use sc_consensus_babe::BabeApi;
 use sp_inherents::CreateInherentDataProviders;
