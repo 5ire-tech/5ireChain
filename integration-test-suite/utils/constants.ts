6162c379bedbc9aa158be96518655e6af20d6249
dung5ire <91842764+dung5ire@users.noreply.github.com>
2024-04-01 15:19:06 +0700

    Refactor code and optimize packages  (#100)
    Signed-off-by: dung5ire <dung@5ire.org>

--- integration-test-suite/utils/constants.ts
+++ integration-test-suite/utils/constants.ts
@@ -12,3 +12,5 @@ export const ETH_BLOCK_POV_LIMIT = 5 * 1024 * 1024; // The same configuration as
 export const GENESIS_ACCOUNT = "0xd43593c715fdd31c61141abd04a99fd6822c8558";
 export const GENESIS_ACCOUNT_BALANCE = "19342813113834066795298815";
 export const EXISTENTIAL_DEPOSIT = 0; // The minimum amount required to keep an account open
+
+export const CHAIN_ID = 997;
