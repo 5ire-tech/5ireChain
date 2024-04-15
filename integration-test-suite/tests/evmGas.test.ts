3bc861411981ddbef01c86cfa659ed01b9850599
dung5ire <dung@5ire.org>
2024-04-15 16:55:14 +0700

    add tests with block gas limit
    Signed-off-by: dung5ire <dung@5ire.org>

--- integration-test-suite/tests/evmGas.test.ts
+++ integration-test-suite/tests/evmGas.test.ts
@@ -1,5 +1,13 @@
 import Web3 from "web3";
-import { BLOCK_TIME, SECONDS, GENESIS_ACCOUNTS, TEST_CONTRACT_ADDRESS, TEST_ACCOUNT } from "../utils/constants";
+import {
+  BLOCK_TIME,
+  SECONDS,
+  GENESIS_ACCOUNTS,
+  TEST_CONTRACT_ADDRESS,
+  TEST_ACCOUNT,
+  ETH_BLOCK_GAS_LIMIT,
+  GENESIS_ACCOUNT_0_PRIVATE_KEY,
+} from "../utils/constants";
 import {
   customRequest,
   killNodeForTestEVM,
@@ -10,7 +18,6 @@ import { sleep } from "../utils/setup";
 
 import { expect } from "chai";
 let web3: Web3;
-
 
 const TRANSFER_VALUE = "1";
 const ERC20_ABI = require("./contracts/MyToken.json").abi;
@@ -48,7 +55,8 @@ describe("EVM related Gas using web3js/ethersjs", function () {
 
   it("estimate gas for contract call", async function () {
     const erc20Contract = new web3.eth.Contract(
-      ERC20_ABI, TEST_CONTRACT_ADDRESS
+      ERC20_ABI,
+      TEST_CONTRACT_ADDRESS
     );
 
     let amount = web3.utils.toWei(TRANSFER_VALUE, "ether");
@@ -60,16 +68,94 @@ describe("EVM related Gas using web3js/ethersjs", function () {
 
   it("estimate gas with gasPrice value is 0x0 ", async function () {
     let result = await web3.eth.estimateGas({
-        from: GENESIS_ACCOUNTS[0],
-        data: ERC20_BYTECODES,
-        gasPrice: "0x0",
+      from: GENESIS_ACCOUNTS[0],
+      data: ERC20_BYTECODES,
+      gasPrice: "0x0",
     });
     expect(result).to.equal(894198);
     result = await web3.eth.estimateGas({
-        from: GENESIS_ACCOUNTS[0],
-        data: ERC20_BYTECODES,
+      from: GENESIS_ACCOUNTS[0],
+      data: ERC20_BYTECODES,
     });
     expect(result).to.equal(894198);
-});
+  });
+
+  it("tx gas limit below ETH_BLOCK_GAS_LIMIT", async function () {
+    const erc20Contract = new web3.eth.Contract(ERC20_ABI);
+
+    const deployTx = erc20Contract.deploy({
+      data: ERC20_BYTECODES,
+      arguments: [],
+    });
+
+    const gasPrice = await web3.eth.getGasPrice();
+
+    const tx = await web3.eth.accounts.signTransaction(
+      {
+        from: GENESIS_ACCOUNTS[0],
+        data: deployTx.encodeABI(),
+        gas: ETH_BLOCK_GAS_LIMIT - 1,
+        gasPrice,
+      },
+      GENESIS_ACCOUNT_0_PRIVATE_KEY
+    );
+    const createReceipt = await customRequest(web3, "eth_sendRawTransaction", [
+      tx.rawTransaction,
+    ]);
+    expect((createReceipt as any).transactionHash).to.be.not.null;
+    expect((createReceipt as any).blockHash).to.be.not.null;
+  });
+
+  it("tx gas limit equal ETH_BLOCK_GAS_LIMIT", async function () {
+    const erc20Contract = new web3.eth.Contract(ERC20_ABI);
+
+    const deployTx = erc20Contract.deploy({
+      data: ERC20_BYTECODES,
+      arguments: [],
+    });
+
+    const gasPrice = await web3.eth.getGasPrice();
+
+    const tx = await web3.eth.accounts.signTransaction(
+      {
+        from: GENESIS_ACCOUNTS[0],
+        data: deployTx.encodeABI(),
+        gas: ETH_BLOCK_GAS_LIMIT,
+        gasPrice,
+      },
+      GENESIS_ACCOUNT_0_PRIVATE_KEY
+    );
+    const createReceipt = await customRequest(web3, "eth_sendRawTransaction", [
+      tx.rawTransaction,
+    ]);
+    expect((createReceipt as any).transactionHash).to.be.not.null;
+    expect((createReceipt as any).blockHash).to.be.not.null;
+  });
+
+  it("tx gas limit larger ETH_BLOCK_GAS_LIMIT", async function () {
+    const erc20Contract = new web3.eth.Contract(ERC20_ABI);
+
+    const deployTx = erc20Contract.deploy({
+      data: ERC20_BYTECODES,
+      arguments: [],
+    });
+
+    const gasPrice = await web3.eth.getGasPrice();
+
+    const tx = await web3.eth.accounts.signTransaction(
+      {
+        from: GENESIS_ACCOUNTS[0],
+        data: deployTx.encodeABI(),
+        gas: ETH_BLOCK_GAS_LIMIT + 1,
+        gasPrice,
+      },
+      GENESIS_ACCOUNT_0_PRIVATE_KEY
+    );
+    const createReceipt = await customRequest(web3, "eth_sendRawTransaction", [
+      tx.rawTransaction,
+    ]);
+    expect((createReceipt as any).error.message).to.equal("exceeds block gas limit");
+  });
+
 
 });
