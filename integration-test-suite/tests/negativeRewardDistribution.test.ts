c4947889f528e39a2bb810b6e11c478adb9ff728
dung5ire <dung@5ire.org>
2024-05-16 23:04:26 +0700

    feat: update reward distribution
    Signed-off-by: dung5ire <dung@5ire.org>

--- integration-test-suite/tests/negativeRewardDistribution.test.ts
+++ integration-test-suite/tests/negativeRewardDistribution.test.ts
@@ -21,18 +21,31 @@ describe("Negative Reward Distribution tests", function () {
     await spawnNodes();
   });
 
-  it("Negative test Reward Distribution with Already Claimed with certain era ", async () => {
+  it("Negative test Reward Distribution with NoSuchValidator  ", async () => {
     const { alice, aliceStash } = await init();
-    // wait to new era 
+    // wait to new era
     await waitNfinalizedBlocks(polkadotApi, 45, 1000);
-    // Valid payout transaction
+
+    // Payout fail with invalid validator 
+    await payoutInValidValidator(alice);
+    
+    await waitNfinalizedBlocks(polkadotApi, 2, 1000);
+
+  });
+
+  it("Negative test Reward Distribution with Already Claimed ", async () => {
+    const { alice, aliceStash } = await init();
+    // wait to new era
+    await waitNfinalizedBlocks(polkadotApi, 45, 1000);
+
+    // // Valid payout transaction
     await payoutSuccess(alice, aliceStash);
 
     await waitForEvent(api, "reward", "Rewarded");
-    // Invalid payout transaction Already Claimed
+    // // Invalid payout transaction Already Claimed
     await payoutAlreadyClaimed(alice, aliceStash);
 
-    await waitNfinalizedBlocks(polkadotApi, 2, 1000);
+    // await waitNfinalizedBlocks(polkadotApi, 2, 1000);
   });
 
   after(async () => {
@@ -48,45 +61,38 @@ async function init() {
   return { alice, aliceStash, bobStash };
 }
 
-async function getCurrentEra() {
-    const currentEra = await api.query.session.currentIndex();
-    console.log("\n: Current Era:", currentEra);
-    return currentEra;
-  }
-
 // Payout Transaction
 // alice : stash account
 // alice_stash : controller account
 
 async function payoutSuccess(alice: KeyringPair, aliceStash: KeyringPair) {
-    console.log(`\n Payout Success`); 
-    const payout = await api.tx.reward.getRewards(aliceStash.address);
-  
-    const transaction = new Promise<{}>(async (resolve, reject) => {
-      const unsub = await payout.signAndSend(
-        alice,
-        { tip: 2000, nonce: -1 },
-        (result) => {
-          console.log(`Payout transaction is ${result.status}`);
-          if (result.status.isInBlock) {
-            console.log(
-              `Payout Transaction included at blockHash ${result.status.asInBlock}`
-            );
-            console.log(`Waiting for finalization... (can take a minute)`);
-          } else if (result.status.isFinalized) {
-            unsub();
-            resolve({});
-          }
+  console.log(`\n Payout Success`);
+  const payout = await api.tx.reward.getRewards(aliceStash.address);
+
+  const transaction = new Promise<{}>(async (resolve, reject) => {
+    const unsub = await payout.signAndSend(
+      alice,
+      { tip: 2000, nonce: -1 },
+      (result) => {
+        console.log(`Payout transaction is ${result.status}`);
+        if (result.status.isInBlock) {
+          console.log(
+            `Payout Transaction included at blockHash ${result.status.asInBlock}`
+          );
+          console.log(`Waiting for finalization... (can take a minute)`);
+        } else if (result.status.isFinalized) {
+          unsub();
+          resolve({});
         }
-      );
-    });
-    
-  }
+      }
+    );
+  });
+}
 
-
-async function payoutAlreadyClaimed(alice: KeyringPair, aliceStash: KeyringPair) {
-    console.log(`\n Payout fail due to AlreadyClaimed`);  
-    const payout = await api.tx.reward.getRewards(aliceStash.address);
+// Alice is is not validator 
+async function payoutInValidValidator(alice: KeyringPair) {
+  console.log(`\n Payout InValid Validator`);
+  const payout = await api.tx.reward.getRewards(alice.address);
 
   const transaction = new Promise<{}>(async (resolve, reject) => {
     const unsub = await payout.signAndSend(
@@ -102,10 +108,13 @@ async function payoutAlreadyClaimed(alice: KeyringPair, aliceStash: KeyringPair)
         } else if (result.status.isFinalized) {
           const data = JSON.stringify(result.events);
           const dataStr = JSON.parse(data);
-          if (result.dispatchError){
-            const filteredData = dataStr.filter((item: any) => item.event.index === "0x0001");
-  
-            expect(filteredData[0].event.data[0].module.token).to.equal("AlreadyClaimed");// AlreadyClaimed
+          if (result.dispatchError) {
+            const filteredData = dataStr.filter(
+              (item: any) => item.event.index === "0x0001"
+            );
+            expect(filteredData[0].event.data[0].module.error).to.equal(
+              "0x00000000"
+            ); // NoSuchValidator
           }
 
           unsub();
@@ -114,9 +123,43 @@ async function payoutAlreadyClaimed(alice: KeyringPair, aliceStash: KeyringPair)
       }
     );
   });
-  
 }
 
+async function payoutAlreadyClaimed(
+  alice: KeyringPair,
+  aliceStash: KeyringPair
+) {
+  console.log(`\n Payout fail due to AlreadyClaimed`);
+  const payout = await api.tx.reward.getRewards(aliceStash.address);
 
+  const transaction = new Promise<{}>(async (resolve, reject) => {
+    const unsub = await payout.signAndSend(
+      alice,
+      { tip: 2000, nonce: -1 },
+      (result) => {
+        console.log(`Payout transaction is ${result.status}`);
+        if (result.status.isInBlock) {
+          console.log(
+            `Payout Transaction included at blockHash ${result.status.asInBlock}`
+          );
+          console.log(`Waiting for finalization... (can take a minute)`);
+        } else if (result.status.isFinalized) {
+          const data = JSON.stringify(result.events);
+          const dataStr = JSON.parse(data);
+          if (result.dispatchError) {
+            const filteredData = dataStr.filter(
+              (item: any) => item.event.index === "0x0001"
+            );
 
+            expect(filteredData[0].event.data[0].module.error).to.equal(
+              "0x00000000"
+            ); // AlreadyClaimed
+          }
 
+          unsub();
+          resolve({});
+        }
+      }
+    );
+  });
+}
--- integration-test-suite/tests/rewardDistribution.test.ts
+++ integration-test-suite/tests/rewardDistribution.test.ts
@@ -13,50 +13,65 @@ import { waitForEvent, waitNfinalizedBlocks } from "../utils/setup";
 const keyring = new Keyring({ type: "sr25519" });
 
 // We should test within 5 eras  ( 200 blocks)
-
+let rewardAddress: string;
 describe("Reward Distribution tests", function () {
   this.timeout(300 * BLOCK_TIME);
 
   before(async () => {
     await spawnNodes();
+    const { alice, aliceStash } = await init();
+    const rewardAccount = await getRewardAccount();
+    rewardAddress = rewardAccount.toString();
+    await transfer(alice, rewardAddress )
+
   });
 
   it("Should test Reward Distribution with Reliability score ", async () => {
     const { alice, aliceStash } = await init();
-    const eraZero = (await getCurrentEra()).toString();
     const reliabilityZero = await getReliabilityScore(aliceStash);
     console.log("Reliability Score in Era 0 without Esg Score:", reliabilityZero);
     await waitNfinalizedBlocks(polkadotApi, 45, 1000);
-    const eraZeroValidatorsReward = await getErasValidatorReward(eraZero);
+    const eraZeroValidatorsReward = await getErasValidatorReward(aliceStash.address);
     console.log("Validator Reward in Era 0 without Esg Score:", eraZeroValidatorsReward.toHuman());
 
-    const eraOne = (await getCurrentEra()).toString();
     const reliabilityOne = await getReliabilityScore(aliceStash);
     console.log("Reliability Score in Era 1 without Esg Score:", reliabilityOne);
 
     await waitNfinalizedBlocks(polkadotApi, 45, 1000);
-    const eraOneValidatorsReward = await getErasValidatorReward(eraOne);
+    const eraOneValidatorsReward = await getErasValidatorReward(aliceStash.address);
 
     console.log("Validator Reward in Era 1 without Esg Score:", eraOneValidatorsReward.toHuman());
 
-    // expect(
-    //   BigInt(eraOneValidatorsReward?.toString()) >
-    //     BigInt(eraZeroValidatorsReward?.toString())
-    // ).true;
+    expect(
+      BigInt(eraOneValidatorsReward?.toString()) >
+        BigInt(eraZeroValidatorsReward?.toString())
+    ).true;
 
-    const eraTwo = (await getCurrentEra()).toString();
     const reliabilityTwo = await getReliabilityScore(aliceStash);
     console.log("Reliability Score in Era 2 without Esg Score:", reliabilityTwo);
     await waitNfinalizedBlocks(polkadotApi, 45, 1000);
-    const eraTwoValidatorsReward = await getErasValidatorReward(eraTwo);
+    const eraTwoValidatorsReward = await getErasValidatorReward(aliceStash.address);
     console.log("Validator Reward in Era 2 without Esg Score:{}", eraTwoValidatorsReward.toHuman());
 
-    // expect(
-    //   BigInt(eraTwoValidatorsReward?.toString()) >
-    //     BigInt(eraOneValidatorsReward?.toString())
-    // ).true;
+    expect(
+      BigInt(eraTwoValidatorsReward?.toString()) >
+        BigInt(eraOneValidatorsReward?.toString())
+    ).true;
 
     await waitNfinalizedBlocks(polkadotApi, 2, 1000);
+    // @ts-ignore
+    const {data: rewardBalanceBeforeClaimByValidator} =  await api.query.system.account(rewardAddress);
+    expect(rewardBalanceBeforeClaimByValidator.free.toBigInt()).to.equal(BigInt("1000000000000000000000"));
+
+    await getReward(alice, aliceStash.address);
+    await waitNfinalizedBlocks(polkadotApi, 45, 1000);
+    const eraThreeValidatorsReward = await getErasValidatorReward(aliceStash.address);
+    console.log("Validator Reward in Era 3 without Esg Score:{}", eraThreeValidatorsReward.toHuman());
+    // @ts-ignore
+    const {data: rewardBalanceAfterClaimByValidator} =  await api.query.system.account(rewardAddress);
+    expect(rewardBalanceAfterClaimByValidator.free.toBigInt()).to.equal(BigInt("688000000000000000000"));
+
+
   });
 
   after(async () => {
@@ -85,38 +100,35 @@ describe("Reward Distribution tests with Reliability score and sustainability sc
     await registerOracle(alice, aliceStash);
     await insertEsgScores(aliceStash, aliceStash, jsonData);
 
-    const eraZero = (await getCurrentEra()).toString();
     const reliabilityZero = await getReliabilityScore(aliceStash);
     console.log("Reliability Score in Era 0 within Esg Score:", reliabilityZero);
     await waitNfinalizedBlocks(polkadotApi, 45, 1000);
-    const eraZeroValidatorsReward = await getErasValidatorReward(eraZero);
+    const eraZeroValidatorsReward = await getErasValidatorReward(aliceStash.address);
     console.log("Validator Reward in Era 0 within Esg Score:", eraZeroValidatorsReward.toHuman());
 
-    const eraOne = (await getCurrentEra()).toString();
     const reliabilityOne = await getReliabilityScore(aliceStash);
     console.log("Reliability Score in Era 1 within Esg Score:", reliabilityOne);
 
     await waitNfinalizedBlocks(polkadotApi, 45, 1000);
-    const eraOneValidatorsReward = await getErasValidatorReward(eraOne);
+    const eraOneValidatorsReward = await getErasValidatorReward(aliceStash.address);
 
     console.log("Validator Reward in Era 1 within Esg Score:", eraOneValidatorsReward.toHuman());
 
-    // expect(
-    //   BigInt(eraOneValidatorsReward?.toString()) >
-    //     BigInt(eraZeroValidatorsReward?.toString())
-    // ).true;
+    expect(
+      BigInt(eraOneValidatorsReward?.toString()) >
+        BigInt(eraZeroValidatorsReward?.toString())
+    ).true;
 
-    const eraTwo = (await getCurrentEra()).toString();
     const reliabilityTwo = await getReliabilityScore(aliceStash);
     console.log("Reliability Score in Era 2 within Esg Score:", reliabilityTwo);
     await waitNfinalizedBlocks(polkadotApi, 45, 1000);
-    const eraTwoValidatorsReward = await getErasValidatorReward(eraTwo);
+    const eraTwoValidatorsReward = await getErasValidatorReward(aliceStash.address);
     console.log("Validator Reward in Era 2 within Esg Score:", eraTwoValidatorsReward.toHuman());
 
-    // expect(
-    //   BigInt(eraTwoValidatorsReward?.toString()) >
-    //     BigInt(eraOneValidatorsReward?.toString())
-    // ).true;
+    expect(
+      BigInt(eraTwoValidatorsReward?.toString()) >
+        BigInt(eraOneValidatorsReward?.toString())
+    ).true;
 
     await waitNfinalizedBlocks(polkadotApi, 2, 1000);
   });
@@ -141,9 +153,14 @@ async function getReliabilityScore(aliceStash: KeyringPair) {
   return reliabilityScores;
 }
 
-async function getErasValidatorReward(era: string) {
-  const reward = await api.query.staking.erasValidatorReward(era);
+async function getErasValidatorReward(validator: string) {
+  const reward = await api.query.reward.validatorRewardAccounts(validator);
   return reward;
+}
+
+async function getRewardAccount() {
+  const rewardAccount = await api.query.reward.rewardAccount();
+  return rewardAccount;
 }
 
 async function getCurrentEra() {
@@ -151,6 +168,7 @@ async function getCurrentEra() {
   console.log("\n: Current Era:", currentEra);
   return currentEra;
 }
+
 
 export async function insertEsgScores(
   aliceStash: KeyringPair,
@@ -201,3 +219,51 @@ export async function registerOracle(
   const oracleAccounts = await api.query.esgScore.sudoOraclesStore();
   console.log(`Account verified in the oracle storage: ${oracleAccounts}`);
 }
+
+
+export async function transfer(
+  alice: KeyringPair,
+  rewardAccount: string,
+) {
+  console.log(`\n Transfering coin to reward account.`);
+  // Transfer 100 5ire to reward Account
+  const transaction = polkadotApi.tx.balances.transfer(rewardAccount, "1000000000000000000000");
+
+  const unsub = await transaction.signAndSend(
+    alice,
+    { tip: 200, nonce: -1 },
+    (result) => {
+      if (result.status.isInBlock) {
+      } else if (result.status.isFinalized) {
+        const data = JSON.stringify(result.events);
+      }
+    }
+  );
+
+  await waitForEvent(api, "balances", "Transfer");
+
+}
+
+export async function getReward(
+  alice: KeyringPair,
+  validator: string,
+) {
+  console.log(`\n Transfering coin to reward account.`);
+  // Transfer 100 5ire to reward Account
+  const transaction = polkadotApi.tx.reward.getRewards(validator);
+
+  const unsub = await transaction.signAndSend(
+    alice,
+    { tip: 200, nonce: -1 },
+    (result) => {
+      if (result.status.isInBlock) {
+      } else if (result.status.isFinalized) {
+        const data = JSON.stringify(result.events);
+      }
+    }
+  );
+
+  await waitForEvent(api, "reward", "Rewarded");
+
+}
+
