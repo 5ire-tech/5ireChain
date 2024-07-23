ccef7c3d6f4ab876179c93d7db15240d30362594
Ayushi <ayushi@5ire.org>
2024-07-23 13:32:02 +0530

    Change thunder ED
    Signed-off-by: Ayushi <ayushi@5ire.org>

--- runtime/firechain-thunder-runtime/src/lib.rs
+++ runtime/firechain-thunder-runtime/src/lib.rs
@@ -450,7 +450,7 @@ impl pallet_indices::Config for Runtime {
 }
 
 parameter_types! {
-	pub const ExistentialDeposit: Balance = DOLLARS/10; 
+	pub const ExistentialDeposit: Balance = DOLLARS/1; 
 	// For weight estimation, we assume that the most locks on an individual account will be 50.
 	// This number may need to be adjusted in the future if this assumption no longer holds true.
 	pub const MaxLocks: u32 = 50;
