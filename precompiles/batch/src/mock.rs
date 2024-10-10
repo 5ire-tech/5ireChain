d6a0397a3f8499870e333fdbba53fecf443d6962
dung5ire <91842764+dung5ire@users.noreply.github.com>
2024-10-10 14:39:30 +0700

    Batch Precompile Feature (#192)
    Signed-off-by: dung5ire <dung@5ire.org>

---
+++ precompiles/batch/src/mock.rs
@@ -0,0 +1,225 @@
+
+use super::*;
+
+use frame_support::traits::Everything;
+use frame_support::{
+	construct_runtime, parameter_types, traits::FindAuthor, weights::Weight, ConsensusEngineId,
+};
+use pallet_evm::{
+	EnsureAddressNever, EnsureAddressRoot, IsPrecompileResult, Precompile, PrecompileHandle,
+	PrecompileResult, PrecompileSet, IdentityAddressMapping
+};
+use precompile_utils::{mock_account, testing::MockAccount};
+use sp_core::H256;
+use sp_runtime::BuildStorage;
+use sp_runtime::{
+	traits::{BlakeTwo256, IdentityLookup},
+	Perbill,
+};
+
+pub type AccountId = MockAccount;
+pub type Balance = u128;
+
+type Block = frame_system::mocking::MockBlockU32<Runtime>;
+
+construct_runtime!(
+	pub enum Runtime	{
+		System: frame_system,
+		Balances: pallet_balances,
+		Evm: pallet_evm,
+		Timestamp: pallet_timestamp,
+		Authorship: pallet_authorship
+	}
+);
+
+parameter_types! {
+	pub const BlockHashCount: u32 = 250;
+	pub const MaximumBlockWeight: Weight = Weight::from_parts(1024, 1);
+	pub const MaximumBlockLength: u32 = 2 * 1024;
+	pub const AvailableBlockRatio: Perbill = Perbill::one();
+	pub const SS58Prefix: u8 = 42;
+}
+
+impl frame_system::Config for Runtime {
+	type BaseCallFilter = Everything;
+	type DbWeight = ();
+	type RuntimeOrigin = RuntimeOrigin;
+	type Nonce = u64;
+	type Block = Block;
+	type RuntimeCall = RuntimeCall;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = AccountId;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type RuntimeEvent = RuntimeEvent;
+	type BlockHashCount = BlockHashCount;
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = pallet_balances::AccountData<Balance>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type BlockWeights = ();
+	type BlockLength = ();
+	type SS58Prefix = SS58Prefix;
+	type OnSetCode = ();
+	type MaxConsumers = frame_support::traits::ConstU32<16>;
+}
+parameter_types! {
+	pub const ExistentialDeposit: u128 = 0;
+}
+impl pallet_balances::Config for Runtime {
+	type MaxReserves = ();
+	type ReserveIdentifier = [u8; 4];
+	type MaxLocks = ();
+	type Balance = Balance;
+	type RuntimeEvent = RuntimeEvent;
+	type DustRemoval = ();
+	type ExistentialDeposit = ExistentialDeposit;
+	type AccountStore = System;
+	type WeightInfo = ();
+	type RuntimeHoldReason = ();
+	type FreezeIdentifier = ();
+	type MaxFreezes = ();
+	type MaxHolds = ();
+}
+
+pub struct MockPrecompileSet<Runtime>(PhantomData<Runtime>);
+
+impl<R: pallet_evm::Config> PrecompileSet for MockPrecompileSet<R> {
+	/// Tries to execute a precompile in the precompile set.
+	/// If the provided address is not a precompile, returns None.
+	fn execute(&self, handle: &mut impl PrecompileHandle) -> Option<PrecompileResult> {
+		let address = handle.code_address();
+
+		if address == Batch.into() {
+			return Some(crate::BatchPrecompile::<R>::execute(handle));
+		}
+
+		None
+	}
+
+	/// Check if the given address is a precompile. Should only be called to
+	/// perform the check while not executing the precompile afterward, since
+	/// `execute` already performs a check internally.
+	fn is_precompile(&self, address: H160, _gas: u64) -> IsPrecompileResult {
+		IsPrecompileResult::Answer { is_precompile: address == Batch.into(), extra_cost: 0 }
+	}
+}
+
+pub type PCall = BatchPrecompileCall<Runtime>;
+
+
+mock_account!(Batch, |_| MockAccount::from_u64(1));
+mock_account!(Revert, |_| MockAccount::from_u64(2));
+
+const MAX_POV_SIZE: u64 = 5 * 1024 * 1024;
+/// Block storage limit in bytes. Set to 40 KB.
+const BLOCK_STORAGE_LIMIT: u64 = 40 * 1024;
+
+parameter_types! {
+	pub BlockGasLimit: U256 = U256::from(u64::MAX);
+	pub PrecompilesValue: MockPrecompileSet<Runtime> = MockPrecompileSet(PhantomData);
+
+	pub const WeightPerGas: Weight = Weight::from_parts(1, 0);
+	pub GasLimitPovSizeRatio: u64 = {
+		let block_gas_limit = BlockGasLimit::get().min(u64::MAX.into()).low_u64();
+		block_gas_limit.saturating_div(MAX_POV_SIZE)
+	};
+	pub GasLimitStorageGrowthRatio: u64 = {
+		let block_gas_limit = BlockGasLimit::get().min(u64::MAX.into()).low_u64();
+		block_gas_limit.saturating_div(BLOCK_STORAGE_LIMIT)
+	};
+}
+
+impl pallet_evm::Config for Runtime {
+	type FeeCalculator = ();
+	type GasWeightMapping = pallet_evm::FixedGasWeightMapping<Self>;
+	type WeightPerGas = WeightPerGas;
+	type CallOrigin = EnsureAddressRoot<AccountId>;
+	type WithdrawOrigin = EnsureAddressNever<AccountId>;
+	type AddressMapping = IdentityAddressMapping;
+	type Currency = Balances;
+	type RuntimeEvent = RuntimeEvent;
+	type Runner = pallet_evm::runner::stack::Runner<Self>;
+	type PrecompilesType = MockPrecompileSet<Runtime>;
+	type PrecompilesValue = PrecompilesValue;
+	type ChainId = ();
+	type OnChargeTransaction = ();
+	type BlockGasLimit = BlockGasLimit;
+	type BlockHashMapping = pallet_evm::SubstrateBlockHashMapping<Self>;
+	type Author = ();
+	type OnCreate = ();
+	type GasLimitPovSizeRatio = GasLimitPovSizeRatio;
+	type Timestamp = Timestamp;
+	type WeightInfo = pallet_evm::weights::SubstrateWeight<Runtime>;
+}
+
+parameter_types! {
+	pub const MinimumPeriod: u64 = 5;
+}
+impl pallet_timestamp::Config for Runtime {
+	type Moment = u64;
+	type OnTimestampSet = ();
+	type MinimumPeriod = MinimumPeriod;
+	type WeightInfo = ();
+}
+
+mock_account!(Alice, |_| H160::repeat_byte(0xAA).into());
+
+pub struct FindAuthorTruncated;
+impl FindAuthor<MockAccount> for FindAuthorTruncated {
+	fn find_author<'a, I>(_digests: I) -> Option<MockAccount>
+	where
+		I: 'a + IntoIterator<Item = (ConsensusEngineId, &'a [u8])>,
+	{
+		Some(Alice.into())
+	}
+}
+
+impl pallet_authorship::Config for Runtime {
+	type FindAuthor = FindAuthorTruncated;
+	type EventHandler = ();
+}
+
+pub(crate) struct ExtBuilder {
+	// endowed accounts with balances
+	balances: Vec<(AccountId, Balance)>,
+}
+
+impl Default for ExtBuilder {
+	fn default() -> ExtBuilder {
+		ExtBuilder { balances: vec![] }
+	}
+}
+
+impl ExtBuilder {
+	pub(crate) fn with_balances(mut self, balances: Vec<(AccountId, Balance)>) -> Self {
+		self.balances = balances;
+		self
+	}
+
+	pub(crate) fn build(self) -> sp_io::TestExternalities {
+		let mut t = frame_system::GenesisConfig::<Runtime>::default()
+			.build_storage()
+			.expect("Frame system builds valid default genesis config");
+
+		pallet_balances::GenesisConfig::<Runtime> { balances: self.balances }
+			.assimilate_storage(&mut t)
+			.expect("Pallet balances storage can be assimilated");
+
+		let mut ext = sp_io::TestExternalities::new(t);
+		ext.execute_with(|| {
+			System::set_block_number(1);
+			pallet_evm::Pallet::<Runtime>::create_account(
+				Revert.into(),
+				hex_literal::hex!("1460006000fd").to_vec(),
+			);
+		});
+		ext
+	}
+}
+
+pub fn balance(account: impl Into<AccountId>) -> Balance {
+	pallet_balances::Pallet::<Runtime>::usable_balance(account.into())
+}
--- runtime/firechain-qa-runtime/src/precompiles.rs
+++ runtime/firechain-qa-runtime/src/precompiles.rs
@@ -7,7 +7,7 @@ use sp_std::marker::PhantomData;
 use pallet_evm_precompile_modexp::Modexp;
 use pallet_evm_precompile_sha3fips::Sha3FIPS256;
 use pallet_evm_precompile_simple::{ECRecover, ECRecoverPublicKey, Identity, Ripemd160, Sha256};
-
+use pallet_evm_precompile_batch::BatchPrecompile;
 pub struct FrontierPrecompiles<R>(PhantomData<R>);
 
 #[allow(clippy::new_without_default)]
@@ -18,8 +18,8 @@ where
 	pub fn new() -> Self {
 		Self(Default::default())
 	}
-	pub fn used_addresses() -> [H160; 7] {
-		[hash(1), hash(2), hash(3), hash(4), hash(5), hash(1024), hash(1025)]
+	pub fn used_addresses() -> [H160; 8] {
+		[hash(1), hash(2), hash(3), hash(4), hash(5), hash(1024), hash(1025), hash(4096)]
 	}
 }
 impl<R> PrecompileSet for FrontierPrecompiles<R>
@@ -34,6 +34,7 @@ where
 			a if a == hash(3) => Some(Ripemd160::execute(handle)),
 			a if a == hash(4) => Some(Identity::execute(handle)),
 			a if a == hash(5) => Some(Modexp::execute(handle)),
+			a if a == hash(4096) => Some(BatchPrecompile::<R>::execute(handle)),
 			// Non-Frontier specific nor Ethereum precompiles :
 			a if a == hash(1024) => Some(Sha3FIPS256::execute(handle)),
 			a if a == hash(1025) => Some(ECRecoverPublicKey::execute(handle)),
@@ -45,7 +46,7 @@ where
 		IsPrecompileResult::Answer {
 			is_precompile: Self::used_addresses().contains(&address),
 			extra_cost: 0,
-		}
+		}	
 	}
 }
 
